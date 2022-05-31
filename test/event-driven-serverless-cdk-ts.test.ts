6faa63bac515d13ea25a7c95dae93f861c05d4ba
vaspoz <vaspoz@mail.ru>
2022-05-31 16:48:27 +0200

    Initial commit

---
+++ test/event-driven-serverless-cdk-ts.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventDrivenServerlessCdkTs from '../lib/event-driven-serverless-cdk-ts-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event-driven-serverless-cdk-ts-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventDrivenServerlessCdkTs.EventDrivenServerlessCdkTsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
